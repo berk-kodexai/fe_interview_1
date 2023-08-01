4d72d4797d1b1954db2a28bb61c2ce378ee2c17a
Berk Ozzambak <berk.ozzambak@kodex-ai.com>
2023-08-02 01:20:48 +0200

    Setup the main view for the interview

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import "./App.css";
-import { getDataFromAPI } from "./helpers";
 export default function App() {
   return (
     <div className="App">
-      <button onClick={async() => console.log(await getDataFromAPI())}>Make API call</button>
+      <button>Make API call</button>
     </div>
   );
 }
